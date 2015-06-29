f6834c8879ae71847541c02385d6a81a08187e52
R.J. Steinert <rj@rjsteinert.com>
2015-06-28 22:53:49 -0400

    Add database configurator

--- app/scripts/routes/RecipeBakery.js
+++ app/scripts/routes/RecipeBakery.js
@@ -12,7 +12,8 @@ OpkBakery.Routers = OpkBakery.Routers || {};
     	routes: {
     		'': 'whichSensor',
             'configure-sensor': 'configureSensor',
-            'which-database': 'whichDatabase'
+            'which-database': 'whichDatabase',
+            'configure-database': 'configureDatabase',
     	},
     	whichSensor: function() {
     		var sensors = new OpkBakery.Collections.Packages()
@@ -37,6 +38,31 @@ OpkBakery.Routers = OpkBakery.Routers || {};
             console.log('ok')
             OpkBakery.recipe.set('sensorCli', this.cli)
             Backbone.history.navigate('which-database', {trigger:true})
+          })
+        },
+        whichDatabase: function() {
+            var database = new OpkBakery.Collections.Packages()
+            database.params.packageType = 'databases'
+            var packagesTable = new OpkBakery.Views.PackagesTable({
+                collection: database
+            })
+            $('.main').html(packagesTable.el)
+            packagesTable.$el.on('select', function() {
+                var selected = (packagesTable.$el.find('.selected'))[0]
+                OpkBakery.recipe.set('databaseId', $(selected).attr('data-id'))
+                Backbone.history.navigate('configure-database', {trigger:true})
+            })
+            database.fetch()
+        },
+        configureDatabase: function() {
+          var database = new OpkBakery.Models.Package({nid: OpkBakery.recipe.get('databaseId')})
+          var form = new OpkBakery.Views.ConfigurePackage({model: database})
+          $('.main').html(form.el)
+          database.fetch()
+          form.on('configured', function() {
+            console.log('ok')
+            OpkBakery.recipe.set('databaseCli', this.cli)
+            Backbone.history.navigate('how-often', {trigger:true})
           })
         },
 
